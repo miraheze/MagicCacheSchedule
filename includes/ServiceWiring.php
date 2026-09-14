cdae29648aaab34e671430322393523d079595cc
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-09-14 14:15:53 -0600

    Add

---
+++ includes/ServiceWiring.php
@@ -0,0 +1,18 @@
+<?php
+
+declare( strict_types = 1 );
+
+use MediaWiki\Config\ServiceOptions;
+use MediaWiki\Extension\MagicCacheSchedule\CacheScheduler;
+use MediaWiki\MediaWikiServices;
+
+return [
+	'MagicCacheSchedule.CacheScheduler' => static function ( MediaWikiServices $services ): CacheScheduler {
+		return new CacheScheduler(
+			new ServiceOptions(
+				CacheScheduler::CONSTRUCTOR_OPTIONS,
+				$services->getMainConfig()
+			)
+		);
+	},
+];
