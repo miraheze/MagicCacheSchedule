01a3835aa283c07023f469b84da265af615e0076
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-09-14 14:25:23 -0600

    +phan

---
+++ .phan/config.php
@@ -0,0 +1,56 @@
+<?php
+
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+
+$cfg['suppress_issue_types'] = [];
+
+$cfg['plugins'] = array_merge( $cfg['plugins'], [
+	'AddNeverReturnTypePlugin',
+	'AlwaysReturnPlugin',
+	'DeprecateAliasPlugin',
+	'DollarDollarPlugin',
+	'DuplicateConstantPlugin',
+	'EmptyMethodAndFunctionPlugin',
+	'EmptyStatementListPlugin',
+	'FFIAnalysisPlugin',
+	'InlineHTMLPlugin',
+	'InvalidVariableIssetPlugin',
+	'InvokePHPNativeSyntaxCheckPlugin',
+	'LoopVariableReusePlugin',
+	'MoreSpecificElementTypePlugin',
+	'NotFullyQualifiedUsagePlugin',
+	'PHPDocRedundantPlugin',
+	'PHPUnitAssertionPlugin',
+	'PHPUnitNotDeadCodePlugin',
+	'PreferNamespaceUsePlugin',
+	'PrintfCheckerPlugin',
+	'RedundantAssignmentPlugin',
+	'SimplifyExpressionPlugin',
+	'SleepCheckerPlugin',
+	'StrictComparisonPlugin',
+	'StrictLiteralComparisonPlugin',
+	'SuspiciousParamOrderPlugin',
+	'UnknownClassElementAccessPlugin',
+	'UnknownElementTypePlugin',
+	'UnreachableCodePlugin',
+	'UnsafeCodePlugin',
+	'UseReturnValuePlugin',
+] );
+
+// $cfg['plugins'][] = __DIR__ . '/../vendor/miraheze/phan-plugins/NoOptionalParamPlugin.php';
+
+$cfg['analyze_signature_compatibility'] = true;
+$cfg['enable_class_alias_support'] = false;
+$cfg['enable_extended_internal_return_type_plugins'] = true;
+$cfg['error_prone_truthy_condition_detection'] = true;
+$cfg['redundant_condition_detection'] = true;
+$cfg['unused_variable_detection'] = true;
+$cfg['warn_about_relative_include_statement'] = true;
+
+$cfg['strict_method_checking'] = true;
+$cfg['strict_object_checking'] = true;
+$cfg['strict_param_checking'] = true;
+$cfg['strict_property_checking'] = true;
+$cfg['strict_return_checking'] = true;
+
+return $cfg;
