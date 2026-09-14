ec6ea4188d0f83ea014b4f01f5ad1761670982d3
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-09-14 14:15:06 -0600

    Create extension

---
+++ MagicCacheSchedule.i18n.magic.php
@@ -0,0 +1,11 @@
+<?php
+
+declare( strict_types = 1 );
+
+$magicWords = [];
+
+$magicWords['en'] = [
+	'cachedaily' => [ 0, 'cachedaily' ],
+	'cacheuntil' => [ 0, 'cacheuntil' ],
+	'cacheinterval' => [ 0, 'cacheinterval' ],
+];
